6ad4e168f2f22bf27cb5aa4613bd4e977500d2a6
kaekasui <kaekasui_github@gmail.com>
2023-03-22 08:09:48 +0900

    Set task tray icon
    - タスクトレイのアイコンを設定

---
+++ Assets/Scripts/TrayIcon.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using UnityEngine;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+
+public class TrayIcon : MonoBehaviour
+{
+    public Texture2D iconTexture;
+    private NotifyIcon notifyIcon;
+
+    void Start()
+    {
+        notifyIcon = new NotifyIcon();
+
+        // アイコン生成
+        byte[] textureBytes = iconTexture.EncodeToPNG();
+        MemoryStream ms = new MemoryStream(textureBytes);
+        Bitmap bitmap = new Bitmap(ms);
+        Icon icon = Icon.FromHandle(bitmap.GetHicon());
+        notifyIcon.Icon = icon;
+
+        notifyIcon.Visible = true;
+        notifyIcon.Text = "はれぽよカレンダー";
+
+        // コンテキストメニュー作成
+        System.Windows.Forms.ContextMenu menu = new System.Windows.Forms.ContextMenu();
+        menu.MenuItems.Add("終了", OnExit);
+        notifyIcon.ContextMenu = menu;
+    }
+
+    void OnExit(object sender, EventArgs e)
+    {
+        // アプリケーション終了
+        UnityEngine.Application.Quit();
+    }
+
+    void OnApplicationQuit()
+    {
+        // NotifyIconオブジェクトを破棄
+        if (notifyIcon != null)
+        {
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (notifyIcon != null)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+        }
+    }
+}
